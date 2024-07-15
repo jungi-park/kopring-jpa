225385f0be492667bf683e0bc2aa6176eb2fd33b
박준기 <qmqqqqm@gmail.com>
2024-07-15 21:48:20 +0900

    코틀린을 이용해서 기존의 기술스택 구현하기 시작

---
+++ src/main/kotlin/com/example/kopringjpa/KopringJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kopringjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KopringJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KopringJpaApplication>(*args)
+}
