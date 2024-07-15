389ce74add8db4fdac57a551bed6bce9f27bca0c
박준기 <qmqqqqm@gmail.com>
2024-07-16 00:03:00 +0900

    @GeneratedValue strategy 변경

--- src/main/kotlin/com/example/kopringjpa/domin/member/member.kt
+++ src/main/kotlin/com/example/kopringjpa/domin/member/member.kt
@@ -2,7 +2,8 @@ package com.example.kopringjpa.domin.member
 
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
 @Entity
-data class member(@Id @GeneratedValue val id: Long,val name: String)
+data class member(@Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long, val name: String)
